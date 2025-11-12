9edfa1577c511811157e506c122f4b67f2c8fdca
coolight543 <robamiao@gmail.com>
2025-11-12 19:19:25 +0000

    meiyo

--- Checker.java
+++ Checker.java
@@ -2,26 +2,30 @@
 //11 11 2025
 //This code is to check if parenthesis is valid
 
-//Step 1 find the last one ({[]})
-//Step 2 look 1 to the right
-//Step 3 if they match remove both and repeat
+//Step 1 Find the first closed parenthesis
+//Step 2 look 1 to the left, if the open parenthesis match the closed parenthesis, remove both parenthesis
+// if null or not match, print out invalid
+//Step 3 Repeat if true
 //Step 4 if your string is empty it's valid
 public class Checker{
-public static void main(String [] args){
-    check("{[()]}");
-}
-
+String temp = ""; 
+String something = "";
+//precondition: The string must only contains parenthesis (), {}, []
+//postcondition: it would tell you if the parenthesis is used correctly
 public static void check (String s)
 {
     String temp = ""; 
-    int length = s.length();
-    int i = 0; 
     String something = "";
-    while (i<length)
+    int length = s.length();
+    for (int i = 0; i<length; i++)
     {
         if (s.substring(0,1).equals(")") || s.substring(0,1).equals("]") || s.substring(0,1).equals("}"))
         {
             System.out.println("Not Valid");
+        }
+        if (s.substring(0,1).equals(""))
+        {
+            System.out.println("Valid");
         }
         // finds the first closing bracket
         else if (s.substring(i,i+1).equals(")") || s.substring(i,i+1).equals("]") || s.substring(i,i+1).equals("}"))
@@ -32,10 +36,19 @@ public static void check (String s)
         if ((temp.equals("(") && something.equals(")")) || (temp.equals("[") && something.equals("]")) || (temp.equals("{") && something.equals("}")) )
         {
             System.out.println(s.substring(0,i-1) + s.substring(i+1));
-            i = -1;
         }
-        i++;
     }
+    if (length != 0)
+    {
+        System.out.println("not Valid");
+    }
+    else 
+    {
+        System.out.println("valid");
+    }
+}
+public static void main(String [] args){
+    check("()");
 }
 }
 // find s the previous open bracket i-1, i, make sure it can pair up with the closing bracket i, i+1
